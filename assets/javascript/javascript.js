b030d327213087f15005b1eeb09c9036e9c95b07
kwong <baldricwat@gmail.com>
2019-02-06 16:40:13 -0800

    API work in progress

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -2,18 +2,51 @@ console.log("hello world");
 
 function animalQuery() {
 
-    var queryURL = "http://api.petfinder.com/pet.find?format=json&key=890ea03f874b500ce1b9db838148bb69&animal=dog&breed="+breed+"location="+zip;
-      
+    var queryURL = "http://api.petfinder.com/pet.find?format=json&key=890ea03f874b500ce1b9db838148bb69&animal=dog&breed=Akita&location=94530";
+
+
 
     $.ajax({
       url: queryURL,
       type: "get",
       dataType: "jsonp",
       }).then(function(response) {
-            console.log('cry', response);
-      });
-    
-};
+            console.log('response', response);
+            // $("#testing").text(JSON.stringify(response));
+            console.log(response);
 
-var location = "https://www.petfinder.com/member/us/ca/berkeley/home-at-last-animal-rescue"+ shelterID;          
+
+
+            // $(document).on('click',".flip-card",function(){
+                  for (var n=0; n<=10; n++){
+                  // var petAge = $("<div>");
+                  // petAge.attr("id","petInfo");
+                  // petAge.text(JSON.stringify(response.petfinder.pets.pet[n]));
+                  // console.log(petAge);
+                 
+
+                  var petAge = $("<div>");
+                  petAge.attr("id","petInfo");
+                  petAge.addClass("petdiv");
+                  petAge.text(JSON.stringify(response.petfinder.pets.pet[n].age.$t));
+                  console.log(petAge);
+                  $("#testing").append(petAge);
+
+                  // var eachPet = $("<div>");
+                  // eachPet.attr("id","petInfo");
+                  // eachPet.text(JSON.stringify(response.petfinder.pets.pet[n]));
+                  // console.log(petAge);
+                  // $("#testing").append(petAge);
+                  
+                  }
+                  });
+
+            
+      // });
+}   
+
+
+
+
+// var location = "https://www.petfinder.com/member/us/ca/berkeley/home-at-last-animal-rescue"+ shelter     ID;          
 animalQuery();
