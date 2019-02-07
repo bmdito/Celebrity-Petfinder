11f630c4db34eadf61010441d53165257687766d
bdiddy <44068902+bmdito@users.noreply.github.com>
2019-02-06 16:39:30 -0800

    additional css, new results and landing

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -2,13 +2,13 @@ console.log("hello world");
 
 function animalQuery() {
 
-    var queryURL = "http://api.petfinder.com/breed.list?format=json&key=890ea03f874b500ce1b9db838148bb69&animal=dog";
+    var queryURL = "https://cors-anywhere.herokuapp.com/http://api.petfinder.com/pet.find?format=json&key=890ea03f874b500ce1b9db838148bb69&animal=dog&count=10&breed="+BREED+ "location="+ZIPCODE;
 
 
     $.ajax({
       url: queryURL,
       type: "get",
-      dataType: "jsonp",
+      dataType: "json",
       }).then(function(response) {
             console.log('cry', response);
       });
